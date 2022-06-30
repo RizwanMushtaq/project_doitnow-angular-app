87632b97090dbe42a9ef4eb1083027ef830f8a6c
RizwanMushtaq <rizwan.mushtaq@ottonova.de>
2022-06-30 17:41:41 +0200

    upgrade to angular 14 (#8)

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from './login.service';
 import { LoginComponent } from './login.component';
 import { AuthModule } from '../auth/auth.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, AuthModule],
+  imports: [CommonModule, ReactiveFormsModule, AuthModule, AppRoutingModule],
   providers: [LoginService],
   exports: [],
 })
