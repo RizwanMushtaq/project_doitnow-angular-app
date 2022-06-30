4eff712695b248ece1615f25a1561268efd482a0
RizwanMushtaq <rizwan.mushtaq@ottonova.de>
2022-06-30 08:58:20 +0200

    moving http module imports to app module (#7)

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { AuthService } from './auth.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   providers: [AuthService],
-  imports: [HttpClientModule],
+  imports: [],
 })
 export class AuthModule {}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,19 +4,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginService } from './login.service';
 import { LoginComponent } from './login.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from '../auth/auth.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AuthModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, AuthModule],
   providers: [LoginService],
   exports: [],
 })
